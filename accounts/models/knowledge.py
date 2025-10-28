1193dc39c712593fc970d17606b24d1881bdfc0c
Mahdi <mahdiashtian.mo@gmail.com>
2025-10-28 13:34:37 +0330

    refactor(knowledge): switch to post_save signal for summary generation

--- accounts/models/knowledge.py
+++ accounts/models/knowledge.py
@@ -1,4 +1,7 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.db import models
+from rest_framework import serializers
 
 from accounts.tasks import generate_summary_task
 
@@ -16,9 +19,17 @@ class Knowledge(models.Model):
         self._skip_summary_update = skip_summary_update
 
         super().save(*args, **kwargs)
-
-        if not self._skip_summary_update:
-            generate_summary_task.delay(self.id)
+        # channel_layer = get_channel_layer()
+        # async_to_sync(channel_layer.group_send)(
+        #     f"online_users",
+        #     {
+        #         "type": "send_message",
+        #         "data": type('TmpSerializer', (serializers.ModelSerializer,), {'Meta': type('Meta', (), {'model': Knowledge, 'fields': '__all__'})})(self).data
+        #
+        #     }
+        # )
+        # if not self._skip_summary_update:
+        #     generate_summary_task.delay(self.id)
 
     def __str__(self):
         return f"Knowledge #{self.pk}"
--- accounts/signals.py
+++ accounts/signals.py
@@ -10,8 +10,28 @@
 # 1. Uncomment this signal block.
 # 2. Comment out or remove the Celery call in Knowledge.save().
 # -------------------------------------------------------------
-# @receiver(post_save, sender=Knowledge)
-# def handle_knowledge_save(sender, instance, **kwargs):
-#     if getattr(instance, "_skip_summary_update", False):
-#             return
-#     generate_summary_task.delay(instance.id)
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework import serializers
+
+from accounts.models import Knowledge
+from accounts.tasks import generate_summary_task
+
+
+@receiver(post_save, sender=Knowledge)
+def handle_knowledge_save(sender, instance, **kwargs):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        f"online_users",
+        {
+            "type": "send_message",
+            "skip_summary_update": instance._skip_summary_update,
+            "data": type('TmpSerializer', (serializers.ModelSerializer,),
+                         {'Meta': type('Meta', (), {'model': Knowledge, 'fields': '__all__'})})(instance).data
+
+        }
+    )
+    if not instance._skip_summary_update:
+        generate_summary_task.delay(instance.id)
