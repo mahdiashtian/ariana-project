f8caf5fa275050ba686e7c3a56a7a44914303c70
Mahdi <mahdiashtian.mo@gmail.com>
2025-10-28 13:50:22 +0330

    refactor(knowledge): switch to post_save signal, keep inline serialization as alternative

--- accounts/models/knowledge.py
+++ accounts/models/knowledge.py
@@ -1,9 +1,4 @@
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
 from django.db import models
-from rest_framework import serializers
-
-from accounts.tasks import generate_summary_task
 
 
 class Knowledge(models.Model):
@@ -19,13 +14,24 @@ class Knowledge(models.Model):
         self._skip_summary_update = skip_summary_update
 
         super().save(*args, **kwargs)
+
+        # ------------------------------------------------------------
+        # Note:
+        # 1. It is possible to perform summary update directly inside the model.
+        #    If you decide to uncomment this inline section, make sure to comment
+        #    out the post_save signal that handles summary generation to avoid duplicate execution.
+        #
+        # 2. Instead of serializing the full object with a temporary ModelSerializer,
+        #    you can use a simpler approach, e.g., sending only {"id": self.id} to the channel
+        #    and let the consumer fetch and serialize the object if needed.
+        # ------------------------------------------------------------
+
         # channel_layer = get_channel_layer()
         # async_to_sync(channel_layer.group_send)(
-        #     f"online_users",
+        #     "online_users",
         #     {
         #         "type": "send_message",
         #         "data": type('TmpSerializer', (serializers.ModelSerializer,), {'Meta': type('Meta', (), {'model': Knowledge, 'fields': '__all__'})})(self).data
-        #
         #     }
         # )
         # if not self._skip_summary_update:
